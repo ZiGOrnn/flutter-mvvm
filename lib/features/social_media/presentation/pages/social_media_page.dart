2efab0eb4e41b405df48d130d04bde081ce1afd1
naluebet <naluebet.m@gmail.com>
2022-11-09 14:02:56 +0700

    feat: creat SocialMediaPage

---
+++ lib/features/social_media/presentation/pages/social_media_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SocialMediaPage extends StatelessWidget {
+  const SocialMediaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
